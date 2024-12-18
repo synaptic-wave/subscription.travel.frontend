be8a2600878f117b74fcab8e153fcfe042212a2a
Otabek <otabektuit11@gmail.com>
2024-12-18 12:30:30 +0500

    fix

--- src/modules/EasyMembership/Register/components/Steps/Information/RegistrationList/index.jsx
+++ src/modules/EasyMembership/Register/components/Steps/Information/RegistrationList/index.jsx
@@ -1,32 +1,16 @@
-import KakaoIcon from "@/assets/icons/kakao.svg?react";
-import GoogleIcon from "@/assets/icons/google.svg?react";
-import MembershipIcon from "@/assets/icons/membership.svg?react";
-
-export const RegistrationList = () => {
+export const RegistrationList = ({ items, onSelect }) => {
   return (
     <div className="w-full flex items-center mt-[110px] gap-4 flex-col min-h-[60vh]">
-      <button className="w-100% w-[375px] px-4 py-3 border border-[#EAEAF4] flex items-center justify-center relative">
-        <span className="absolute left-4">
-          <KakaoIcon />
-        </span>
-        <span className="text-[15px] font-normal">
-          카카오톡 계정으로 회원가입
-        </span>
-      </button>
-
-      <button className="w-100% w-[375px] px-4 py-3 border border-[#EAEAF4] flex items-center justify-center relative">
-        <span className="absolute left-4">
-          <GoogleIcon />
-        </span>
-        <span className="text-[15px] font-normal">구글 계정으로 회원가입</span>
-      </button>
-
-      <button className="w-100% w-[375px] px-4 py-3 border border-[#EAEAF4] flex items-center justify-center relative">
-        <span className="absolute left-4">
-          <MembershipIcon />
-        </span>
-        <span className="text-[15px] font-normal">일반 회원가입</span>
-      </button>
+      {items.map((item, idx) => (
+        <button
+          key={idx}
+          className="w-100% w-[375px] px-4 py-3 border border-[#EAEAF4] flex items-center justify-center relative"
+          onClick={() => onSelect(item.value)}
+        >
+          <span className="absolute left-4">{item.icon}</span>
+          <span className="text-[15px] font-normal">{item.name}</span>
+        </button>
+      ))}
     </div>
   );
 };
--- src/modules/EasyMembership/Register/components/Steps/Information/index.jsx
+++ src/modules/EasyMembership/Register/components/Steps/Information/index.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import { RegistrationList } from "./RegistrationList";
+import KakaoIcon from "@/assets/icons/kakao.svg?react";
+import GoogleIcon from "@/assets/icons/google.svg?react";
+import MembershipIcon from "@/assets/icons/membership.svg?react";
+import { KakaoMembership } from "./MembershipTypes/Kakao";
+
+const memberTypes = {
+  KAKAO: "kakao",
+  GOOGLE: "google",
+  GENERAL: "general"
+};
+
+const registrationsList = [
+  {
+    icon: <KakaoIcon />,
+    name: "카카오톡 계정으로 회원가입",
+    value: memberTypes.KAKAO
+  },
+  {
+    icon: <GoogleIcon />,
+    name: "구글 계정으로 회원가입",
+    value: memberTypes.GOOGLE
+  },
+  {
+    icon: <MembershipIcon />,
+    name: "일반 회원가입",
+    value: memberTypes.GENERAL
+  }
+];
+
+const Components = {
+  [memberTypes.KAKAO]: KakaoMembership
+};
 
 export const MemberInformation = () => {
+  const [memberType, setMemberType] = useState();
+
+  const handleSelectMembership = (val) => {
+    setMemberType(val);
+  };
+
+  const MembershipComponent = Components[memberType];
+
+  if (memberType) {
+    return <MembershipComponent />;
+  }
+
   return (
-    <>
-      <RegistrationList />
-    </>
+    <RegistrationList
+      items={registrationsList}
+      onSelect={handleSelectMembership}
+    />
   );
 };
